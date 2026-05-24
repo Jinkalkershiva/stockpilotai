d5f43b38546aeadfc20682eb6036f06628f3401a
jinka <jinkalkers@gmail.com>
2026-05-24 19:37:09 +0530

    Initial commit

---
+++ src/main/java/com/jinkalker/stockpilotai/StockpilotaiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jinkalker.stockpilotai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockpilotaiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockpilotaiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinkalker/stockpilotai/StockpilotaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinkalker.stockpilotai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockpilotaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
